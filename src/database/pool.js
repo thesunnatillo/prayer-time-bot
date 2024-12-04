fe7dc306bc470570943c26e90b57202719e2ac03
thesunnatillo <sunnatillocoder@gmail.com>
2024-12-04 10:49:26 +0500

    ssl

--- src/database/pool.js
+++ src/database/pool.js
@@ -7,7 +7,7 @@ module.exports = new Pool({
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
