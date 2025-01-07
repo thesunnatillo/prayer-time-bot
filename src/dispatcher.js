4a572fd8267c5b36912889b0f5c7303ff99e81b5
thesunnatillo <sunnatillocoder@gmail.com>
2025-01-07 23:06:49 +0500

    faet: add stat command to dispatcher

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -5,11 +5,13 @@ const saveUser = require("./handlers/saveUser")
 const getToday = require("./handlers/today")
 const getWeek = require("./handlers/weekly")
 const botInfo = require("./handlers/bot_info")
+const getUsersCount = require("../src/handlers/users_count")
 
 const dispatcher = new Composer();
 
 dispatcher.command('start', startHandler)
 dispatcher.command('help', helpHandler)
+dispatcher.command('stat', getUsersCount)
 dispatcher.hears('📆 Bugun', getToday)
 dispatcher.hears('🗓 Haftalik', getWeek)
 dispatcher.hears('❗️ Bot haqida', botInfo)
