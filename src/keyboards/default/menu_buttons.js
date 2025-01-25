bc1b1d5c8a680d6cc4873f83f001e08aa6238b7b
thesunnatillo <sunnatillocoder@gmail.com>
2025-01-25 15:34:08 +0500

    qazolarni hisoblamaslik

--- src/keyboards/default/menu_buttons.js
+++ src/keyboards/default/menu_buttons.js
@@ -1,2 +1,2 @@
 const {Keyboard} = require("grammy");
-module.exports = new Keyboard().text('📆 Bugun').text('🗓 Haftalik').row().text('‼️ Qazo namozlar').row().text('❗️ Bot haqida').resized()
+module.exports = new Keyboard().text('📆 Bugun').text('🗓 Haftalik').row().text('❗️ Bot haqida').resized()
